9337253abaa6f00c3a332f54cada42ad1aa7f714
juliosf06 <juliosf06@gmail.com>
2016-02-23 17:10:41 -0500

    Add ignore

--- models.py
+++ models.py
@@ -2,9 +2,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-class Segment(models.Model):
-    nombre = models.Charfield(max_length=100)
+class Segmento(models.Model):
+    nombre = models.CharField(max_length=100)
 
-class Campana(models.Model)
-    codigo_campana = models.Charfield(max_length=100)
+class Campana(models.Model):
+    codigo_campana = models.CharField(max_length=100)
     
