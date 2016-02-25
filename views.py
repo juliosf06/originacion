a56eb9b08f431c9a57d88843b91b8c8f8796920a
juliosf06 <juliosf06@gmail.com>
2016-02-25 15:11:25 -0500

    Fix var static

--- views.py
+++ views.py
@@ -2,10 +2,11 @@
 from django.http import HttpResponse
 from django.template import RequestContext
 from django.shortcuts import render_to_response as render
+from django.conf import settings
 from models import *
 
 def index(request):
     campanas = Campana.objects.filter(mes_vigencia='201512')
-    print campanas
+    static_url=settings.STATIC_URL
     return render('reports/reports.html', locals(),
                   context_instance=RequestContext(request))
