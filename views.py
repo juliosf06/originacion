bc668523a5edc0e4d06fa72c2b284dd57a75ebaf
juliosf06 <julio.sotelo@bbva.com>
2016-11-03 09:36:01 -0500

    se modifico meses mora 9

--- views.py
+++ views.py
@@ -1468,7 +1468,7 @@ def seguimiento_tarjeta(request, filtro1='mes_vigencia', filtro2='2011'):
         num_lista = len(menor2015_list)
         num_mora4 = 3 #4
         num_mora6 = 5 #6
-        num_mora9 = 9 #10
+        num_mora9 = 8 #10
         num_mora12 = 11 #12
 
     morames_list = []
@@ -2254,7 +2254,7 @@ def seguimiento_pld(request, filtro1='mes_vigencia', filtro2='2011'):
         num_lista = len(menor2015_list)
         num_mora4 = 3 #4
         num_mora6 = 5 #6
-        num_mora9 = 9 #10
+        num_mora9 = 8 #10
         num_mora12 = 11 #12
 
     morames_list = []
