e0f4473dc8a2309e7f5fceea6b64330a134e00d7
juliosf06 <julio.sotelo@bbva.com>
2016-12-13 11:12:32 -0500

    se agrego tema a los comentarios

--- models.py
+++ models.py
@@ -484,6 +484,7 @@ class Moras(models.Model):
 
 class Comentario(models.Model):
     periodo = models.CharField(max_length=10)
+    titulo = models.CharField(max_length=400, default=0)
     comentario = models.CharField(max_length=3500, default=0)
     usuario = models.CharField(max_length=20)
     tiempo = models.CharField(max_length=50, default=0)
--- views.py
+++ views.py
@@ -4752,10 +4752,11 @@ def comentario(request, filtro1='1', filtro2='1', filtro3='1'):
                 username = request.user.username
                 periodo = request.POST.get('periodo')
                 hora1 = datetime.now().strftime("%H:%M:%S")
+                title = request.POST.get('titulo')
                 comentario = request.POST.get('comentarios')
                 comentario = comentario.encode('utf-8')
                 comentario2 = str(comentario.replace('\r\n','<br/>'))
-                comentario_instance = Comentario.objects.create(usuario=username,periodo=periodo,comentario=comentario2, tiempo=hoy, hora=hora1 )
+                comentario_instance = Comentario.objects.create(usuario=username,titulo=title,periodo=periodo,comentario=comentario2, tiempo=hoy, hora=hora1 )
                 comentario_instance2 = ComentarioBackup.objects.create(usuario=username,periodo=periodo,comentario=comentario2, tiempo=hoy, hora=hora1 ) 
                 comments = Comentario.objects.values('usuario','tiempo').order_by('usuario')
     else:
